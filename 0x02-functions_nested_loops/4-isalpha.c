92b12d2bd98ac09861f94422359bd5bfda480a6a
MabuzaMP <mpumelelombz9@gmail.com>
2023-06-20 18:19:02 -0700

    C functions and Nested loop

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,9 +2,10 @@
 /**
  * is_alpha -Shows 1 if a letter
  * is shown,then 0 for rest
+ * @c: the charracter to be checked
  * Return: 1 for letters. 0 for the rest
  */
-int _isalpha(int c) 
+int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
