20cfdd7878e6f169adf2acd063b1f2741d2813d9
MabuzaMP <mpumelelombz9@gmail.com>
2023-07-10 11:19:22 -0700

    ARGc/v Corrections

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,8 +6,11 @@
  *@argv: The array of arguments passed into the program.
  *Return: 0 on success, or 1 on failure.
  */
+
 int main(int argc, char *argv[])
 {
+	int i;
+
 for (int i = 0; i < argc; i++)
 {
 printf("%s\n", argv[i]);
