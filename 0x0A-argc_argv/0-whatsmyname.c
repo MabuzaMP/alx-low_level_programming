bee578c2795076ea580fa3cde7037f54f0e0ce1a
MabuzaMP <mpumelelombz9@gmail.com>
2023-07-10 06:50:19 -0700

    ARGV Correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
  * @argv: the argument value for char
  * Return: Always 0 ( success)
  */
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
+	(void) argc;
 printf("%s\n", argv [argc -argc]);
 return 0;
 }
