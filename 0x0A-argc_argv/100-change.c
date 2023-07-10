fa7b8becb0a9e7b10391cb9665ca88e7876acea7
MabuzaMP <mpumelelombz9@gmail.com>
2023-07-10 12:56:35 -0700

    Correection 2

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,41 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define COIN_VALUES {25, 10, 5, 2, 1}
-#define NUM_COINS 5
-
 /**
- * get_min_coins: Prints the minimum number of coins needed to make change for an amount of money.
- * @cents: The amount of cents to make change for.
- * Return :The minimum number of coins needed.
- */
-
-int get_min_coins(int cents)
-{
-int min_coins = 0;
-for (int i = 0; i < NUM_COINS; i++)
-{
-int coin_value = COIN_VALUES[i];
-int num_coins = cents / coin_value;
-min_coins += num_coins;
-cents -= num_coins * coin_value;
-}
-return min_coins;
-}
+ * main - Main Entry
+ * @argc: input
+ * @argv: input
+ * Return: Always 0 (success)
+**/
 int main(int argc, char *argv[])
 {
-if (argc != 2)
+int cents, coins = 0;
+
+if (argc == 2)
+{
+cents = atoi(*(argv + 1));
+while (cents > 0)
+{
+if (cents % 25 < cents)
+{
+cents -= 25;
+coins++;
+}
+else if (cents % 10 < cents)
+{
+cents -= 10;
+coins++;
+}
+else if (cents % 5 < cents)
+{
+cents -= 5;
+coins++;
+}
+else if (cents % 2 < cents)
+{
+cents -= 2;
+coins++;			}
+else if (cents % 1 < cents)
+{
+cents -= 1;
+coins++;
+}
+}
+}
+else
 {
 printf("Error\n");
-return 1;
+return (1);
 }
-int cents = atoi(argv[1]);
-if (cents < 0)
-{
-printf("0\n");
-return 0;
-}
-int min_coins = get_min_coins(cents);
-printf("%d\n", min_coins);
-return 0;
+printf("%d\n", coins);
+return (0);
 }
