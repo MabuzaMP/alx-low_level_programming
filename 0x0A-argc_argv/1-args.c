3ebce085a7832c01a168796bfa9e2018647a30bb
MabuzaMP <mpumelelombz9@gmail.com>
2023-07-10 07:32:52 -0700

    ARGc/v Corrections

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * Prints the number of arguments passed into the program.
- * @param argc The number of arguments passed into the program.
- * @param argv The array of arguments passed into the program.
- * @return 0 on success, or 1 on failure.
+ * main - Prints the number of arguments passed into the program.
+ * @argc: The number of arguments passed into the program.
+ * @argv: The array of arguments passed into the program.
+ * Return: 0 on success, or 1 on failure.
  */
 int main(int argc, char *argv[])
 {
-	(void) argv;
+(void) argv;
 printf("%d\n", argc - 1);
-return 0;
+return (0);
 }
