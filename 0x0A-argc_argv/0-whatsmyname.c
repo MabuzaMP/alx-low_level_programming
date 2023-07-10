baae6e4e7390792f02f9e1d448408d191af2623c
MabuzaMP <mpumelelombz9@gmail.com>
2023-07-10 06:44:49 -0700

    ARGV Correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
+
 /**
  * main- function that print the name of file
  * @argc: the argument counter for char
  * @argv: the argument value for char
  * Return: Always 0 ( success)
  */
-
-int main(int argc, char *argv[])
+int main(char argc, char *argv[])
 {
-
-printf("%s\n", argv #include <stdio.h>
-/**
- * main- function that print the name of file
- * @argc: the argument counter for char
- * @argv: the argument value for char
- * Return: Always 0 ( success)
- */
-
-int main(int argc, char *argv[])
-{
-
-printf("%d\n", argv [argc -argc]);
+printf("%s\n", argv [argc -argc]);
 return 0;
 }
