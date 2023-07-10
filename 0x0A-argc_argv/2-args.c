c2704f4d1cf5c3bd87b1e7a96f0cbed7c2d5e7a8
MabuzaMP <mpumelelombz9@gmail.com>
2023-07-10 07:40:44 -0700

    ARGc/v Corrections

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,13 +2,13 @@
 
 /**
  *main - Prints all arguments passed into the program
- *argc: The number of arguments passed into the program.
- *argv: The array of arguments passed into the program.
+ *@argc: The number of arguments passed into the program.
+ *@argv: The array of arguments passed into the program.
  *Return: 0 on success, or 1 on failure.
  */
 int main(int argc, char *argv[])
 {
-for (int i = 1; i < argc; i++)
+for (int i = 0; i < argc; i++)
 {
 printf("%s\n", argv[i]);
 }
