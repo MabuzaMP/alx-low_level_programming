e106a4de6c17d3b96c262f08760874cc8d936703
MabuzaMP <mpumelelombz9@gmail.com>
2023-07-18 10:13:02 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef LIKEMACRO_H
+#define LIKEMACRO_H
+/**
+ * File: 3-function_like_macro
+ * @X: input
+ * Desc: header file
+ */
+#define ABS(x) \ ((x) < 0 ? (-(x)) : (x))
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+ 0x0D C Preprocessor Task 18/07/2023
