cca2d86db05c5a7da7ba045ddc56e5cbcaaa7f70
MabuzaMP <mpumelelombz9@gmail.com>
2023-07-28 19:05:29 -0700

    Linked List

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("(a tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
+}
