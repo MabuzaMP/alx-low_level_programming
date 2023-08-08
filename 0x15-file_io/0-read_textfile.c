a3f28314d3b547e0e4739a03c93ddf7a55801416
Mpumelelo Mabuza <132443084+MabuzaMP@users.noreply.github.com>
2023-08-08 04:43:47 +0200

    file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,10 +2,9 @@
 
 /**
  * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
- *
- * Return: numbers of letters printed. It fails, returns 0.
+ * @filename: filename/s.
+ * @letters: No of letters printed.
+ * Return: No of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
